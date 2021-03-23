45341fae6ebaea42a543ddcd4263f420ce0b2555
poornimavithanage <vithanagepurnima@gmail.com>
2021-03-23 17:25:09 +0530

    Add files

---
+++ src/courses/courses.mock.ts
@@ -0,0 +1,23 @@
+export const COURSES = [
+  {
+    id: 1,
+    title: 'NodeJS - The complete guide',
+    description: 'Learn',
+    author: 'Maxim Madli',
+    url: 'https://nestjs.com/',
+  },
+  {
+    id: 2,
+    title: 'ReactJS - The complete guide',
+    description: 'Learn',
+    author: 'Maxim Madli',
+    url: 'https://nestjs.com/',
+  },
+  {
+    id: 3,
+    title: 'Angular - The complete guide',
+    description: 'Learn',
+    author: 'Maxim Madli',
+    url: 'https://nestjs.com/',
+  },
+];
